4039a72849e39a3d4b581131a236622bf3d3c778
Mark <mark.angler@outlook.com>
2023-11-20 20:15:22 -0500

    Update tsTransforms.py

--- tsTransforms.py
+++ tsTransforms.py
@@ -1,15 +1,8 @@
 import pandas as pd
-
+from utilsforecast.preprocessing import fill_gaps
 
 
 def tsGaps(df, dsFreq):
-    df.set_index('ds', inplace=True)
-    df = df.groupby('unique_id').resample(dsFreq).sum()
-
-    # Reset the index - the group keys will be added as columns
-    df.reset_index(inplace=True)
-
-    # Fill NaN values in 'y' column
-    df['y'] = df['y'].fillna(0)
-
+    df = fill_gaps(df, dsFreq)
     return df
+
