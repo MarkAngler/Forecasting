badbb8503fe121f7038c4ee54788f3cd97f4c335
Mark <mark.angler@outlook.com>
2023-11-25 21:26:56 -0500

    finished PoC

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import streamsync as ss
 import dbx
+import pandas as pd
+import tsTransforms as ts
+import selectionLists as sl
+import statsForecast as sf
 
 # This is a placeholder to get you started or refresh your memory.
 # Delete it or adapt it as necessary.
@@ -15,6 +19,7 @@ def update_dataSourceStateEle(state,payload):
         state["uploadFileVisible"] = False
 
 def dbxConnect(state):
+    state["dbxConnectionMessage"] = True
     try:
         # dbx.connect(state['dbxJdbcStateEle'],state['dbxPat'])
         state['dbxHost'], state['dbxHttpPath'], state['dbxCatalog'] = dbx.splitConnString(state['dbxJdbcStateEle'])
@@ -25,8 +30,55 @@ def dbxConnect(state):
         state['dbxConnectionStateMessage'] = f"-{str(e)}"
 
 def dbxGetTableColumns(state):
-    state['dbxTableColumns'] = dbx.getColumns(state['_dbxCursor'],state['dbxCatalog'] ,state['dbxSchemaTableStateEle'])
+    dbxTableColumns = dbx.getColumns(state['_dbxCursor'],state['dbxCatalog'] ,state['dbxSchemaTableStateEle'])
+    columnDict = {}
+    for column in dbxTableColumns:
+        columnDict[column] = column
+    state['TableColumns'] = columnDict
+    state['selectColumnsVisible'] = True
 
+def dbxGetTableData(state):
+    dfList = dbx.read(state['_dbxConn'], 
+                           state['_dbxCursor'], 
+                           state['dbxCatalog'], 
+                           state['dbxSchemaTableStateEle'], 
+                           state['unique_id'], 
+                           state['ds'], 
+                           state['y'])
+    state['df'] = pd.DataFrame(dfList, columns=['unique_id','ds','y'])
+    state['dfVisible'] = True
+
+def setDfFreq(state):
+    state['df'] = ts.tsGaps(state['df'],state['dsFreq'])
+
+def evaluate(state):
+    state['dfTrain'], state['dfTest'] = ts.splitDf(state['df'],state['horizon'])
+    sForecastEval = sf.statsForecast(state['dfTrain'],season_length=12,freq='M')
+    state['dfTestForecast'] = sForecastEval.forecast(h=state['horizon'])
+    state['dfTestForecast'] = state['dfTestForecast'].merge(state['dfTest'], on=['ds', 'unique_id'], how='inner')
+
+    dfAggregated = state['dfTestForecast'].groupby(['ds']).sum().reset_index()
+
+    jsonAggregated = dfAggregated.to_dict(orient='records')
+
+    state['jsonTestForecast'] = {
+        "data":[ 
+            {
+                'x': [record['ds'] for record in jsonAggregated],
+                'y': [record['y'] for record in jsonAggregated],
+                'mode': 'lines',
+                'type': 'scatter'
+            }
+        ]
+    }
+
+    state['dfTestForecastVisible'] = True
+
+
+
+
+def predict(state):
+    pass
 
 initial_state = ss.init_state({
     "my_app": {
@@ -35,6 +87,7 @@ initial_state = ss.init_state({
     "errorMessages": None,
     "uploadFileVisible": False,
     "dataSourceStateEle": None,
+    "dfVisible": False,
     "dbxConnVisible": False,
     "dbxJdbcStateEle": None,
     "dbxPat": None,
@@ -42,11 +95,25 @@ initial_state = ss.init_state({
     "_dbxConn": None,
     "dbxConnectionStateMessage":None,
     "dbxConnected": False,
+    "dbxConnectionMessage": None,
     "dbxSchemaTableStateEle": None,
     "dbxHost": None,
     "dbxHttpPath": None,
     "dbxCatalog": None,
-    "dbxTableColumns": None,
+    "TableColumns": None,
+    "selectColumnsVisible": False,
+    "ds": None,
+    "unique_id": None,
+    "y": None,
+    "df": None,
+    "dfTrain": None,
+    "dfTest": None,
+    "dfTestForecastVisible": False,
+    "dfTestForecast": None,
+    "jsonTestForecast": None,
+    "dsFreqSelectionList": sl.frequency_dict,
+    "dsFreq": 'M',
+    "horizon": 6,
     
 })
 
